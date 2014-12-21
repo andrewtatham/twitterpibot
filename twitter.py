b29f5b5fe934ccd87430662208c099aa1c8faf35
Andrew Tatham <adrewtatham@gmail.com>
2014-12-21 20:49:50 +0000

    intial commit

---
+++ twitter.py
@@ -0,0 +1,59 @@
+
+
+
+# APP_KEY = 'xglqZ20zTPe97yq0ZLkQsV2af'
+# APP_SECRET = 'ci0fovVfpRrxuqP9C0KwAxl1tajH8yJoxQcSuUNYluFBxpS9Au'
+
+from twython import Twython
+import time
+import os.path
+import pickle
+
+def Authenticate():
+    APP_KEY = 'xglqZ20zTPe97yq0ZLkQsV2af'
+    APP_SECRET = 'ci0fovVfpRrxuqP9C0KwAxl1tajH8yJoxQcSuUNYluFBxpS9Au'
+
+    exists = os.path.isfile('FINAL_OAUTH_TOKEN.pkl') and os.path.isfile('FINAL_OAUTH_TOKEN_SECRET.pkl')
+
+    if (exists):
+
+        FINAL_OAUTH_TOKEN = pickle.load(open("FINAL_OAUTH_TOKEN.pkl", "rb"))
+        FINAL_OAUTH_TOKEN_SECRET = pickle.load(open("FINAL_OAUTH_TOKEN_SECRET.pkl", "rb"))
+
+    else:
+
+        twitter = Twython(APP_KEY, APP_SECRET)
+
+        auth = twitter.get_authentication_tokens()
+
+        OAUTH_TOKEN = auth['oauth_token']
+        OAUTH_TOKEN_SECRET = auth['oauth_token_secret']
+
+        print auth['auth_url']
+
+        oauth_verifier = raw_input('Enter your pin:')
+
+        twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+        final_step = twitter.get_authorized_tokens(oauth_verifier)
+
+        FINAL_OAUTH_TOKEN = final_step['oauth_token']
+        FINAL_OAUTH_TOKEN_SECRET = final_step['oauth_token_secret']
+
+        pickle.dump(FINAL_OAUTH_TOKEN, open("FINAL_OAUTH_TOKEN.pkl", "wb"))
+        pickle.dump(FINAL_OAUTH_TOKEN_SECRET, open("FINAL_OAUTH_TOKEN_SECRET.pkl", "wb"))
+   
+    twitter = Twython(APP_KEY, APP_SECRET, FINAL_OAUTH_TOKEN, FINAL_OAUTH_TOKEN_SECRET)
+
+    return twitter
+
+    
+
+twitter = Authenticate()
+
+while (True):
+    timeline = twitter.get_home_timeline()
+    for tweet in timeline:
+        print tweet['text']
+    time.sleep(60)
+    
