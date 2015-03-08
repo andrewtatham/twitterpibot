a99991ee81ba5df5daf22ad277cd1632c62b3a26
Andrew Tatham <andrewtatham@gmail.com>
2015-03-08 19:06:25 +0000

    piglow

--- twitterstream.py
+++ twitterstream.py
@@ -54,6 +54,9 @@ import textwrap
 import picamera
 import picamera.array
 
+from piglow import PiGlow
+import math
+
 def ReplyWithPhoto(sender):
     logging.info("taking photo...")
     path = TakePhoto()
@@ -545,6 +548,26 @@ def WebcamTasks():
             logging.exception(e.message, e.args)             
             pprint.pprint(e)
     
+def PiglowTasks():
+
+    while running:
+        try:
+            for t in range(360):
+                for led in range(18):
+                    piglow.led(led+1, pattern[led][t])
+                if running:
+                    sleep(1) 
+                else:
+                    break
+        except Exception as e:
+            logging.exception(e.message, e.args)             
+            pprint.pprint(e)
+
+
+def getLed(arm,colour):
+    return int(6*arm+colour)
+def getBright(factor):
+    return max(0, min(int(-0.5 * maxbright + maxbright * factor),255))
 
 
 logging.basicConfig(filename='twitter.log',level=logging.INFO)
@@ -603,6 +626,8 @@ thread_list = [
 ##    threading.Thread(target=MonitorTasks),
 ##    threading.Thread(target=HourlyTasks),
 ##    threading.Thread(target=FifteenMinuteTasks),    
+
+    threading.Thread(target=PiglowTasks),    
     threading.Thread(target=PicameraTasks),    
     threading.Thread(target=WebcamTasks),    
     threading.Thread(target=StreamTweets)]
@@ -628,6 +653,17 @@ for i in range(5):
 windowname = "Image"
 window = cv2.namedWindow(windowname)
 
+
+piglow = PiGlow()
+maxbright = 32
+piglow.all(0)
+pattern = [[0 for x in range(360)]for y in range(18)]
+for t in range(360):
+    for colour in range(6):
+        for arm in range(3):
+            b1 = math.sin(math.radians(t + arm * 15 +  colour * 360/32))
+            led = getLed(arm,colour)
+            pattern[led][t] = getBright(b1)
 
 tokens = Authenticate()
 twitter = Twython(tokens[0],tokens[1],tokens[2],tokens[3])
