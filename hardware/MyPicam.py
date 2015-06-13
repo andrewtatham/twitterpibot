4aa5846025f2a4a49819ec9e4d7fcb79015ced4f
Andrew Tatham <andrewtatham@gmail.com>
2015-06-13 12:47:15 +0100

    picam and piglow

--- hardware/MyPicam.py
+++ hardware/MyPicam.py
@@ -2,40 +2,40 @@
 from Camera import Camera, Photo
 import time
 
-enablePicam = True
 
 try:
     import picamera
     import picamera.array
+
 except Exception as e:
     print(e.message)
-    enablePicam = False
+  
 
 
 class MyPicam(Camera):
     def __init__(self, *args, **kwargs):
-        if enablePicam:
 
-            try:
-                self.mypicamera = picamera.PiCamera()
-                self.mypicamera.resolution = [320,240]
-                self.mypicamera.start_preview()
-                time.sleep(2)
-                self.mypicamera.stop_preview()
-                self.picamerastream = picamera.array.PiRGBArray(mypicamera) 
-                #picamerawindow = cv2.namedWindow("picamera")
-                self.enabled = True
 
-            except Exception as e:
-                print(e.message)
-                self.enabled = False
-                enablePicam = False
+        try:
+            self.mypicamera = picamera.PiCamera()
+            self.mypicamera.resolution = [320,240]
+            self.mypicamera.start_preview()
+            time.sleep(2)
+            self.mypicamera.stop_preview()
+            self.picamerastream = picamera.array.PiRGBArray(mypicamera) 
+            #picamerawindow = cv2.namedWindow("picamera")
+            self.enabled = True
+
+        except Exception as e:
+            print(e.message)
+            self.enabled = False
+                
     
     
 
 
     def TakePhoto(args):
-        if enablePicam and args.enabled:
+        if args.enabled:
             print('taking photo')
             args.mypicamera.capture(picamerastream, format='bgr', resize=(320,240))
 
@@ -50,7 +50,7 @@ class MyPicam(Camera):
         
 
     def Close(args):
-        if enablePicam and args.enabled:
+        if args.enabled:
             args.mypicamera.close()
             args.picamerastream.close()
             # cv2.namedWindow("picamera")
