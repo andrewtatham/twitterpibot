202ddb38d38531545b936e9bad2cbb124e4fbc7f
Andrew Tatham <andrewtatham@gmail.com>
2015-08-11 22:05:04 +0100

    magic 8-ball reply to questions

--- responses/Magic8BallResponse.py
+++ responses/Magic8BallResponse.py
@@ -45,10 +45,10 @@ class Magic8BallResponse(Response):
     def Condition(args, inboxItem):
         return (
             super(Magic8BallResponse, args).Condition(inboxItem)
-            or 
-            (
-                not inboxItem.from_me #and random.randint(0,4) == 0
-            )
+            #or 
+            #(
+            #    not inboxItem.from_me #and random.randint(0,4) == 0
+            #)
         ) and inboxItem.text.find("?") != -1
 
 
