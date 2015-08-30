95468116f548cf4a59216ef68c7bdc0f80052f1f
Andrew Tatham <andrewtatham@gmail.com>
2015-08-30 08:52:50 +0100

    whitespace

--- users/BotBlocker.py
+++ users/BotBlocker.py
@@ -5,7 +5,7 @@ from MyTwitter import MyTwitter
 class BotBlocker(object):
 	def __init__(self, *args, **kwargs):
 		tooKeen = ["(follow|DM|join) (me|us|back|now)"]
-		pornWords = [ "sexy","naughty","kinky","frisky","bored","cum","horny","housewi(fe|ves?)","teen","latina","ass","boobs?","tits?","puss(y|ies)","milf","hoes?","boot(y|ies)","cam","18+","xxx","slut","babe","dirty","naked","bitch"]
+		pornWords = ["sexy","naughty","kinky","frisky","bored","cum","horny","housewi(fe|ves?)","teen","latina","ass","boobs?","tits?","puss(y|ies)","milf","hoes?","boot(y|ies)","cam","18+","xxx","slut","babe","dirty","naked","bitch"]
 		businessWords = ["team","professionals ","Recruitment", "Training ","Leadership","business","discount","cheap","betting","industry","casino","economic ","entrepreneur","veture","capital","startup","angel","invest","enterprise"]
 		tooKeenRx = re.compile("|".join(tooKeen))
 		businessRx = re.compile("|".join(businessWords))
@@ -13,7 +13,7 @@ class BotBlocker(object):
 		self.rxs = [(tooKeenRx, 5), (pornRx, 4), (businessRx, 1)]
 		return super(BotBlocker, self).__init__(*args, **kwargs)
 	def IsUserBot(self, user_id, username, screen_name, description):
-		print("[Botblock] checking: " +  username  + " [@" + screen_name + "] " +  description)
+		print("[Botblock] checking: " + username + " [@" + screen_name + "] " + description)
 		blockFollower = False
 		score = 0
 		if not description: 
@@ -29,8 +29,7 @@ class BotBlocker(object):
 			blockFollower = True
 		if not blockFollower:
 			with MyTwitter() as twitter:
-				lastTweets = twitter.get_user_timeline(
-					user_id = user_id,
+				lastTweets = twitter.get_user_timeline(user_id = user_id,
 					screen_name = screen_name,
 					trim_user = True,
 					count = 20)
@@ -51,7 +50,6 @@ class BotBlocker(object):
 
 	def BlockUser(self, user_id, screen_name):
 		with MyTwitter() as twitter:
-			twitter.create_block(
-				user_id = user_id,
+			twitter.create_block(user_id = user_id,
 				screen_name = screen_name)
 		print("BLOCKED")
