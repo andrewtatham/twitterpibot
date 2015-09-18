9956fff0bd67b14cd85c9a03f1615e6f77f9f702
Andrew Tatham <andrewtatham@gmail.com>
2015-09-18 18:29:18 +0100

    varied responses

--- responses/FatherTedResponse.py
+++ responses/FatherTedResponse.py
@@ -8,21 +8,66 @@ class FatherTedResponse(Response):
 
     def Respond(args, inboxItem):
 
+        responses = [
+            # exclaimation
+            "WOW!",
+            "ROFL!",
+            "LOL!",
+            "WOWZERS!",
+            "OMG!",
+            "WTF?!",
+            "ZOMG!",
+            
+            # positive
+            "Nice.",
+            "Cool.",
+            "Hah.",
+            "Yeah.",
+            "Indeed.",
+            "Yes.",
+            "Ok.",
+            
+            # neutral
+            "[Shrug]",
+            "Wat.",
+            "Meh.",
+            "Hmpfh.",
+            "Whatever.",
+            "Whatevs.",
 
+            # questions
+            "Who?",
+            "Why?",
+            "What?",
+            "When?",
+
+            # negative
+            "No.",
+            "Nah.",
+            
+            # simpsons
+            "DOH!",
+            "Dont have a cow man!",
+             
+            # Robot Noises
+            "BEEP BOOP."
+            
+            # father ted
+            "That would be an ecumenical matter. #FatherTed",
+            "Careful now. #FatherTed",
+            "Down with this sort of thing! #FatherTed",
+            "Is there anything to be said for another mass? #FatherTed",
+            "That money was just resting in my account! #FatherTed",
+            "as I said last time, it won't happen again. #FatherTed",
+            "These are small... but the ones out there are far away. #FatherTed",
+            "I love my brick! #FatherTed",
+            "Go away! I don't want to catch menopause! #FatherTed"
         #You'll have some tea...  are you sure you don't want any?  Aw go on,
         #you'll have some.  Go on go on go on go on go on go on go on go on GO
         #ON!
-
-        responses = ["That would be an ecumenical matter",
-            "Careful now",
-            "Down with this sort of thing!",
-            "Is there anything to be said for another mass?",
-            "That money was just resting in my account!",
-            "as I said last time, it won't happen again",
-            "These are small... but the ones out there are far away.",
-            "I love my brick!",
-            "Go away! I don't want to catch menopause!"]
-        response = random.choice(responses) + " #FatherTed"
+            
+            ]
+        response = random.choice(responses) 
         return args.ReplyWith(inboxItem, response)
 
         
