44aaa9ac885a76df1b05083d2f94eee23748353e
Andrew Tatham <andrewtatham@gmail.com>
2015-09-19 13:55:18 +0100

    trends and searches

--- schedule/SavedSearchScheduledTask.py
+++ schedule/SavedSearchScheduledTask.py
@@ -6,7 +6,6 @@ from apscheduler.triggers.interval import IntervalTrigger
 from Queue import Queue
 import random
 
-searchColours = cycle([Fore.BLUE, Fore.WHITE])
 
 class SavedSearchScheduledTask(ScheduledTask):
     def __init__(self, *args, **kwargs):
@@ -20,22 +19,13 @@ class SavedSearchScheduledTask(ScheduledTask):
             if args._savedSearches.empty():
                 searches = twitter.get_saved_searches()
                 for search in searches:
-                    colour = searchColours.next()
-                    print(colour + "SavedSearches: [" + search['name'] + "]")
                     args._savedSearches.put(search)
             try:
                 search = args._savedSearches.get()
                 searchTweets = twitter.search(q = search['query'])
                 for searchTweet in searchTweets["statuses"]:
-                    colour = searchColours.next()                    
-                    print(colour + "SavedSearch: [" + search['name'] + "] - " \
-                        + searchTweet["user"]["name"] + " [@" + searchTweet["user"]["screen_name"] + "] - "\
-                        + searchTweet["text"].replace("\n", "   "))
+                    args.context.inbox.put(IncomingTweet(searchTweet, context, source))
 
-                searchTweet = random.choice(searchTweets["statuses"])
-                twitter.retweet(id = searchTweet["id_str"])
-
-                # TODO reply?
             finally:
                 args._savedSearches.task_done()
 
--- schedule/TrendsScheduledTask.py
+++ schedule/TrendsScheduledTask.py
@@ -9,16 +9,14 @@ import urllib
 from collections import Counter
 from OutgoingTweet import OutgoingTweet
 import random
+from IncomingTweet import IncomingTweet
 
-
-worldwide_WOEID = 1
+#worldwide_WOEID = 1
 UK_WOEID = 23424975
 US_WOEID = 23424977
-leeds_WOEID = 26042
+#leeds_WOEID = 26042
 
 woeids = cycle([UK_WOEID, US_WOEID])
-
-trendColours = cycle([Fore.MAGENTA, Fore.WHITE])
 
 class TrendsScheduledTask(ScheduledTask):
     def __init__(self, *args, **kwargs):
@@ -29,32 +27,16 @@ class TrendsScheduledTask(ScheduledTask):
 
     def onRun(args):
         with MyTwitter() as twitter:
-
-
             if args._trendsList.empty():
                 woeid = woeids.next()
                 trends = twitter.get_place_trends(id = woeid)[0].get('trends',[])   
                 for trend in trends:
-                    colour = trendColours.next()
-                    print(colour + "Trends: [" + trend['name'] + "]")
-                args._trendsList.put(trend)
-
+                    args._trendsList.put(trend)
             try:
                 trend = args._trendsList.get()
                 trendtweets = twitter.search(q = urllib.quote_plus(trend["name"]), result_type = "popular")
-
-                trendText = ""
                 for trendtweet in trendtweets["statuses"]:
-                    colour = trendColours.next()
-                    trendText += " " + trendtweet["text"].replace("\n", "   ")
-                    print(colour + "Trend: [" + trend["name"] + "] - " \
-                        + trendtweet["user"]["name"] + " [@" + trendtweet["user"]["screen_name"] + "] - "\
-                        + trendtweet["text"].replace("\n", "   "))
-
-                trendtweet = random.choice(trendtweets["statuses"])
-                twitter.retweet(id = trendtweet["id_str"])
-
-                # TODO reply?
+                    args.context.inbox.put(trendtweet)
             finally:
                 args._trendsList.task_done()
 
