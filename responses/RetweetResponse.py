3ad7af5a9a8a580a9ecec1f1314a3ccf4bb7d9d1
Andrew Tatham <andrewtatham@gmail.com>
2015-09-28 16:15:55 +0100

    changed retweet chance

--- responses/RetweetResponse.py
+++ responses/RetweetResponse.py
@@ -7,9 +7,9 @@ class RetweetResponse(Response):
     def Condition(args, inboxItem):
         return inboxItem.isTweet and not inboxItem.from_me and not inboxItem.to_me and not inboxItem.retweeted and (
             (inboxItem.sender.isBot and random.randint(0,50) == 0) \
-            or (inboxItem.sender.isFriend and random.randint(0,1) == 0) \
+            or (inboxItem.sender.isFriend and random.randint(0,3) == 0) \
             or (inboxItem.sender.isRetweetMore and random.randint(0,9) == 0) \
-            or (inboxItem.sourceIsTrend and random.randint(0,50) == 0)  \
+            or (inboxItem.sourceIsTrend and random.randint(0,20) == 0)  \
             or (inboxItem.sourceIsSearch and random.randint(0,20) == 0) \
             or (random.randint(0,99) == 0)
         )
