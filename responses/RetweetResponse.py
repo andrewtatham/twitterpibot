9fc59613134fb013bced7280f9d96fc72346ecac
Andrew Tatham <andrewtatham@gmail.com>
2015-09-28 16:25:00 +0100

    dont RT "RT 2 win"

--- responses/RetweetResponse.py
+++ responses/RetweetResponse.py
@@ -4,8 +4,29 @@ import random
 from OutgoingTweet import OutgoingTweet
 from MyTwitter import MyTwitter
 class RetweetResponse(Response):
+
+    def __init__(self, *args, **kwargs):
+
+        self.bannedTopics = [
+            "(RT|Retweet) ?(to|2) ?win",
+            # Football
+
+            # Job Adverts
+
+            
+            ]
+
+        self.rx = re.compile("|".join(self.bannedTopics), re.IGNORECASE)
+
+
+        
     def Condition(args, inboxItem):
-        return inboxItem.isTweet and not inboxItem.from_me and not inboxItem.to_me and not inboxItem.retweeted and (
+        return inboxItem.isTweet \
+            and not inboxItem.from_me \
+            and not inboxItem.to_me \
+            and not inboxItem.retweeted \
+            and not bool(args.rx.match(inboxItem.text)) \
+            and (
             (inboxItem.sender.isBot and random.randint(0,50) == 0) \
             or (inboxItem.sender.isFriend and random.randint(0,3) == 0) \
             or (inboxItem.sender.isRetweetMore and random.randint(0,9) == 0) \
