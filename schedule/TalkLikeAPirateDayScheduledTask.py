d83b4164b677c4903e8321a748d1ae64315850b2
Andrew Tatham <andrewtatham@gmail.com>
2015-09-29 11:55:59 +0100

    piracy

--- schedule/TalkLikeAPirateDayScheduledTask.py
+++ schedule/TalkLikeAPirateDayScheduledTask.py
@@ -7,7 +7,16 @@ import random
 
 piracy = [
     # TODO Piraty things
-    "Arrr"
+
+    "You can always trust the untrustworthy because you can always trust that they will be untrustworthy. Its the trustworthy you can’t trust.",
+    "If ye can’t trust a pirate, ye damn well can’t trust a merchant either!",
+    "Yarrrr! there be ony two ranks of leader amongst us pirates! Captain and if your really notorious then it’s Cap’n!",
+    "Not all treasure is silver and gold",
+
+    "There comes a time in most men’s lives where they feel the need to raise the Black Flag",
+    "The rougher the seas, the smoother we sail. Ahoy!",
+    "Drink up me hearties yo-ho-ho! a pirates life for me",
+
 ] 
 class TalkLikeAPirateDayScheduledTask(ScheduledTask):
         
