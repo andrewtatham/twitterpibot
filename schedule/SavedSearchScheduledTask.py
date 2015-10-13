47f911e90ab03a2961ea85a650122170996f11f5
Andrew Tatham <andrewtatham@gmail.com>
2015-10-13 10:54:37 +0100

    replaced queue with list

--- schedule/SavedSearchScheduledTask.py
+++ schedule/SavedSearchScheduledTask.py
@@ -3,30 +3,24 @@ from MyTwitter import MyTwitter
 from colorama import Fore
 from itertools import cycle
 from apscheduler.triggers.interval import IntervalTrigger
-from Queue import Queue
 import random
 import MyQueues
 
 class SavedSearchScheduledTask(ScheduledTask):
     def __init__(self, *args, **kwargs):
-        self._savedSearches = Queue()
+        self._savedSearches = []
 
     def GetTrigger(args):
         return IntervalTrigger(minutes=32)
 
     def onRun(args):
         with MyTwitter() as twitter:
-            if args._savedSearches.empty():
+            if not args._savedSearches:
                 searches = twitter.get_saved_searches()
-                for search in searches:
-                    args._savedSearches.put(search)
-            try:
-                search = args._savedSearches.get()
-                searchTweets = twitter.search(q = search['query'])
-                for searchTweet in searchTweets["statuses"]:
-                    searchTweet['tweetsource'] = "search:" + search["name"]
-                    MyQueues.inbox.put(searchTweet)
+                args._savedSearches.extend(searches)
 
-            finally:
-                args._savedSearches.task_done()
-
+            search = args._savedSearches.pop()
+            searchTweets = twitter.search(q = search['query'])
+            for searchTweet in searchTweets["statuses"]:
+                searchTweet['tweetsource'] = "search:" + search["name"]
+                MyQueues.inbox.put(searchTweet)
