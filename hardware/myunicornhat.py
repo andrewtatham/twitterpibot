e494cc9cdd05dd329a40d1e33a4c6cf98d3c19fc
Andrew Tatham <andrewtatham@gmail.com>
2015-10-13 17:38:50 +0100

    unicorn hat

--- hardware/myunicornhat.py
+++ hardware/myunicornhat.py
@@ -1,24 +1,25 @@
 import random
 import unicornhat
 
+
+buffer = [[(0,0,0) for x in range(8)] for y in range(8)]
+
 class MyUnicornHat(object):
 
     def CameraFlash(args, on):
-
-        r = 0
-        g = 0
-        b = 0
-        if on:
-            r = 255
-            g = 255
-            b = 255
-
         for y in range(8):
             for x in range(8):
+                if on:
+                    r = 255
+                    g = 255
+                    b = 255
+                else:
+                    pixel = buffer[x][y]
+                    r = pixel[0]
+                    g = pixel[1]
+                    b = pixel[2]
                 unicornhat.set_pixel(x,y,r,g,b)
         unicornhat.show()
-
-
 
     def OnInboxItemRecieved(args, inboxItem):
         x = random.randint(0,7)
@@ -26,11 +27,20 @@ class MyUnicornHat(object):
         r = random.randint(1,255)
         g = random.randint(1,255)
         b = random.randint(1,255)
+        buffer[x][y] = (r,g,b)
         unicornhat.set_pixel(x,y,r,g,b)
         unicornhat.show()
 
     def Fade(args):
-        pass
+        for y in range(8):
+            for x in range(8):
+                pixel = buffer[x][y]
+                r = max(pixel[0] - 1, 0)
+                g = max(pixel[1] - 1, 0)
+                b = max(pixel[2] - 1, 0)
+                buffer[x][y] = (r,g,b)
+                unicornhat.set_pixel(x,y,r,g,b)
+        unicornhat.show()
 
     def Close(args):
         r = 0
