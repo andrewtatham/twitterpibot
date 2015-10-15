d410e5f374e4e08337f4a46889c4243ebc998243
Andrew Tatham <andrewtatham@gmail.com>
2015-10-15 21:30:00 +0100

    piglow lock

--- hardware/MyPiglow.py
+++ hardware/MyPiglow.py
@@ -3,44 +3,49 @@ import time
 import random
 
 from PyGlow import PyGlow
+from multiprocessing import Lock
 
-t = 0;
-maxbright = 255
-piglow = PyGlow()
-piglow.all(0)
-buffer = [0 for led in range(18)]
+_maxbright = 255
+_piglow = PyGlow()
+_piglow.all(0)
+_buffer = [0 for led in range(18)]
+_lock = Lock()
 
-def getLed(arm, colour):
+def _getLed(arm, colour):
     return int(6 * arm + colour)
 
-def getBright(factor):
-    return max(0, min(int(-0.5 * maxbright + maxbright * factor),255))
+def _getBright(factor):
+    return max(0, min(int(-0.5 * _maxbright + _maxbright * factor),255))
 
 
 def OnInboxItemRecieved(inboxItem):
-    led = random.randint(0,17)
-    buffer[led] = max(0, min((buffer[led] + 1), 255))
-    WriteLed(led)
+    with _lock:
+        led = random.randint(0,17)
+        _buffer[led] = max(0, min((_buffer[led] + 1), 255))
+        _WriteLed(led)
 
 def Fade():
-    for led in range(18):
-        if buffer[led] > 1:
-            buffer[led] = max(0,buffer[led] - 5)        
-    WriteAll()
+    with _lock:
+        for led in range(18):
+            if _buffer[led] > 1:
+                _buffer[led] = max(0,_buffer[led] - 5)        
+        _WriteAll()
 
-def WriteAll():
+def _WriteAll():
     for led in range(18):
-        WriteLed(led)
+        _WriteLed(led)
 
-def WriteLed( led):
-    bright = max(0, min(buffer[led], 255))
-    piglow.led(led + 1, bright)
+def _WriteLed( led):
+    bright = max(0, min(_buffer[led], 255))
+    _piglow.led(led + 1, bright)
 
 def CameraFlash(on):
-    if on:
-        piglow.color("white", brightness = 255)
-    else:
-        WriteAll()
+    with _lock:
+        if on:
+            _piglow.color("white", brightness = 255)
+        else:
+            _WriteAll()
 
 def Close():    
-    piglow = None
+    with _lock:
+        piglow = None
