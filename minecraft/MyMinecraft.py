30e4ae7a427461b97266dacaee00ecf002f434fc
Andrew Tatham <andrewtatham@gmail.com>
2015-10-16 07:27:33 +0100

    minecraft wip

--- minecraft/MyMinecraft.py
+++ minecraft/MyMinecraft.py
@@ -2,82 +2,45 @@ import mcpi.minecraft as minecraft
 import mcpi.block as block
 import server
 import time
+import hardware
 
-class MyMineCraft(object):
+def TakeScreenshot():
+    if iswindows:
+        WindowsScreenShot(filename)
+    else:
+        RaspPiScreenShot(filename)
 
+def RaspPiScreenShot(filename):
+    ## http://www.raspberrypi-spy.co.uk/2014/05/how-to-capture-minecraft-screenshots-on-the-raspberry-pi/
+    pass
 
+def WindowsScreenShot(filename):
+    from PIL import ImageGrab
+    import win32gui
 
+    toplist, winlist = [], []
+    def enum_cb(hwnd, results):
+        winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
+    win32gui.EnumWindows(enum_cb, toplist)
 
-    def foo(args):
+    firefox = [(hwnd, title) for hwnd, title in winlist if 'firefox' in title.lower()]
+    # just grab the hwnd for first window matching firefox
+    firefox = firefox[0]
+    hwnd = firefox[0]
 
-        if iswindows:
-            mc = minecraft.Minecraft.create( server.address )
-        else:
-            mc = minecraft.Minecraft.create()
-
-
-
-
-
-
-        mc.postToChat("x")
-        for x in range(1,8):
-            mc.setBlock(x,0,0, block.SANDSTONE)
-        mc.postToChat("y")
-        for y in range(1,8):
-            mc.setBlock(0, y, 0, block.GRAVEL)
-        mc.postToChat("z")
-        for z in range(1,8):
-            mc.setBlock(0,0,z, block.WOOD)
-
-
-        mc.setBlock(0,0,0, block.TNT, 1)
-
-
-        # position player/camera
-        mc.player.setPos(8, 0, 8)
-        
-        
-        iswindows = True
-
-        filename = "temp.png"
-
-        # take screenshot
-        if iswindows:
-            WindowsScreenShot(filename)
-        else:
-            RaspPiScreenShot(filename)
+    win32gui.SetForegroundWindow(hwnd)
+    bbox = win32gui.GetWindowRect(hwnd)
+    img = ImageGrab.grab(bbox)
+    img.show()
 
 
-        mc.postToChat("done")
+def Connect():
+    if hardware.iswindows:
+        return minecraft.Minecraft.create( server.address )
+    else:
+        return minecraft.Minecraft.create()
 
-        return filename
-
-    def RaspPiScreenShot(args, filename):
-        ## http://www.raspberrypi-spy.co.uk/2014/05/how-to-capture-minecraft-screenshots-on-the-raspberry-pi/
-        pass
-
-    def WindowsScreenShot(args, filename):
-
-
-        from PIL import ImageGrab
-        import win32gui
-
-        toplist, winlist = [], []
-        def enum_cb(hwnd, results):
-            winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
-        win32gui.EnumWindows(enum_cb, toplist)
-
-        firefox = [(hwnd, title) for hwnd, title in winlist if 'firefox' in title.lower()]
-        # just grab the hwnd for first window matching firefox
-        firefox = firefox[0]
-        hwnd = firefox[0]
-
-        win32gui.SetForegroundWindow(hwnd)
-        bbox = win32gui.GetWindowRect(hwnd)
-        img = ImageGrab.grab(bbox)
-        img.show()
-      
+     
 
 
 if __name__ == "__main__":
--- responses/MinecraftResponse.py
+++ responses/MinecraftResponse.py
@@ -1,14 +1,21 @@
 from Response import Response
-from MyMinecraft import MyMinecraft
+import MyMinecraft
 class MinecraftResponse(Response):
     def Condition(args, inboxItem):
         return super(MinecraftResponse, args).Condition(inboxItem) \
             and args.Contains(inboxItem.words, "minecraft")
     def Respond(args, inboxItem):
 
+        mc = MyMinecraft.Connect()
+
+        pPos = mc.player.getTilePos()
+
+   
+        mc.setBlocks(pPos.x-1,pPos.y,pPos.z-1,pPos.x+1,pPos.y+2,pPos.z+1,block.STONE)
 
 
-        mc = MyMinecraft()
-        filename = mc.foo()
-        args.context.Upl
-        return super(MinecraftResponse, args).Respond(inboxItem)
+        mc.player.setPos(pPos.x,pPos.y+3,pPos.z)
+
+
+        filename = MyMinecraft.TakeScreenshot("temp/mc.png")
+
