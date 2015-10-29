32c51d27d19ffda3532ca34b1b358034061cc3a7
Andrew Tatham <andrewtatham@gmail.com>
2015-10-29 14:23:00 +0000

    streaming

--- twitterpibot/schedule/StreamTrendsScheduledTask.py
+++ twitterpibot/schedule/StreamTrendsScheduledTask.py
@@ -2,13 +2,10 @@ import datetime
 import logging
 
 from apscheduler.triggers.interval import IntervalTrigger
-
-from twitterpibot.outgoing.OutgoingDirectMessage import OutgoingDirectMessage
 from twitterpibot.schedule.ScheduledTask import ScheduledTask
 from twitterpibot.tasks.StreamTweetsTask import StreamTweetsTask
 from twitterpibot.tasks import Tasks
 from twitterpibot.twitter import TwitterHelper, TrendingTopics
-from twitterpibot.twitter.TwitterHelper import Send
 
 logger = logging.getLogger(__name__)
 
@@ -19,36 +16,12 @@ _stop_list = []
 
 class StreamTrendsScheduledTask(ScheduledTask):
     def GetTrigger(self):
-        return IntervalTrigger(minutes=5)
+        return IntervalTrigger(seconds=30)
 
     def onRun(self):
         global _trends_list
         global _start_list
         global _stop_list
-
-        stream_list = Tasks.get()
-
-        if not _trends_list:
-            _trends_list.extend(TrendingTopics.get())
-
-            # Check for streams that are no longer trending
-            for trend in stream_list:
-                is_trending = trend in _trends_list
-                if not is_trending:
-                    logger.debug("adding " + trend + " to stop list")
-                    _stop_list.append(trend)
-
-        if _trends_list:
-            trend = _trends_list.pop()
-
-            logger.debug("checking " + trend)
-            is_streaming = trend in stream_list
-            if is_streaming:
-                logger.debug("adding " + trend + " to stop list")
-                _stop_list.append(trend)
-            else:
-                logger.debug("adding " + trend + " to start list")
-                _start_list.append(trend)
 
         if _stop_list:
             text = "Stopping streams:"
@@ -59,11 +32,34 @@ class StreamTrendsScheduledTask(ScheduledTask):
                 Tasks.remove(stop_trend)
             logger.info(text)
             text += " at " + str(datetime.datetime.now())
-            # Send(OutgoingDirectMessage(text=text))
         elif _start_list:
             # Create stream
             start_trend = _start_list.pop()
             text = "Starting stream: " + start_trend + " at " + str(datetime.datetime.now())
             logger.info(text)
             Tasks.add(StreamTweetsTask(TwitterHelper.GetStreamer(topic=start_trend)))
-            # Send(OutgoingDirectMessage(text=text))
+        else:
+            stream_list = Tasks.get()
+
+            if not _trends_list:
+                _trends_list.extend(TrendingTopics.get())
+
+            if _trends_list and stream_list:
+                # Check for streams that are no longer trending
+                for trend in stream_list:
+                    logger.debug("checking " + trend)
+                    is_trending = trend in _trends_list
+                    if not is_trending:
+                        logger.debug("adding " + trend + " to stop list")
+                        _stop_list.append(trend)
+
+                trend = _trends_list.pop()
+
+                logger.debug("checking " + trend)
+                is_streaming = trend in stream_list
+                if is_streaming:
+                    logger.debug("adding " + trend + " to stop list")
+                    _stop_list.append(trend)
+                else:
+                    logger.debug("adding " + trend + " to start list")
+                    _start_list.append(trend)
--- twitterpibot/twitter/MyStreamer.py
+++ twitterpibot/twitter/MyStreamer.py
@@ -2,10 +2,13 @@ from twython.streaming.api import TwythonStreamer
 import twitterpibot.MyQueues as MyQueues
 import twitterpibot.twitter.Authenticator as Authenticator
 import logging
+import time
 
 logger = logging.getLogger(__name__)
 
 _tokens = None
+
+_back_off = 60
 
 
 class MyStreamer(TwythonStreamer):
@@ -18,13 +21,22 @@ class MyStreamer(TwythonStreamer):
         super(MyStreamer, self).__init__(_tokens[0], _tokens[1], _tokens[2], _tokens[3])
 
     def on_success(self, data):
+        global _back_off
         if self._topic:
             data['tweetsource'] = "stream:" + self._topic
         MyQueues.inbox.put(data)
+        _back_off = 60
 
     def on_error(self, status_code, data):
+        global _back_off
         msg = str(status_code) + " " + str(data)
         logger.error(msg)
+
+        if status_code == 420:
+            logger.warn("backing off for " + str(_back_off) + " seconds")
+
+            time.sleep(_back_off)
+            _back_off *= 2
 
     @property
     def topic(self):
