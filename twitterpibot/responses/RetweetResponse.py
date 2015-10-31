46b5ae0374076a47cdd25ed31356202254161e5e
Andrew Tatham <andrewtatham@gmail.com>
2015-10-31 16:46:46 +0000

    retweet logging

--- twitterpibot/responses/RetweetResponse.py
+++ twitterpibot/responses/RetweetResponse.py
@@ -4,6 +4,8 @@ import re
 from twitterpibot.responses.Response import Response
 from twitterpibot.twitter import TwitterHelper
 
+import logging
+logger = logging.getLogger(__name__)
 
 class RetweetResponse(Response):
     def __init__(self):
@@ -45,4 +47,5 @@ class RetweetResponse(Response):
         return False
 
     def Respond(self, inbox_item):
+        logger.info("retweeting status id %s", inbox_item.status_id)
         TwitterHelper.retweet(inbox_item.status_id)
