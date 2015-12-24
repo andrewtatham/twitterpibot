f4e2421d0065d7302f0d07bd132ad42072bd52ba
Andrew Tatham <andrewtatham@gmail.com>
2015-12-24 15:14:20 +0000

    more responses

--- twitterpibot/responses/FatherTedResponse.py
+++ twitterpibot/responses/FatherTedResponse.py
@@ -12,13 +12,13 @@ class FatherTedResponse(Response):
     def respond(self, inbox_item):
         responses = [
             # exclaimation
-            "WOW!",
-            "ROFL!",
-            "LOL!",
-            "WOWZERS!",
-            "OMG!",
-            "WTF?!",
-            "ZOMG!",
+            # "WOW!",
+            # "ROFL!",
+            # "LOL!",
+            # "WOWZERS!",
+            # "OMG!",
+            # "WTF?!",
+            # "ZOMG!",
 
             # positive
             "Nice.",
@@ -48,7 +48,7 @@ class FatherTedResponse(Response):
             "Nah.",
 
             # simpsons
-            "DOH!",
+            # "DOH!",
             "Dont have a cow man!",
 
             # Robot Noises
@@ -63,10 +63,34 @@ class FatherTedResponse(Response):
             "as I said last time, it won't happen again. #FatherTed",
             "These are small... but the ones out there are far away. #FatherTed",
             "I love my brick! #FatherTed",
-            "Go away! I don't want to catch menopause! #FatherTed"
+            "Go away! I don't want to catch menopause! #FatherTed",
             # You'll have some tea...  are you sure you don't want any?  Aw go on,
             # you'll have some.  Go on go on go on go on go on go on go on go on GO
             # ON!
+
+            # Borat
+            "Niiice!",
+            "What type of dog is this?",
+            "Is this a cat in a hat?",
+            "Are you telling me the man who try to put a rubber fist in my anus was a homosexual?",
+            "Wa-woo-wee-wa!",
+            "Jak sie masz",
+
+            # IT Crowd
+            "Have you tried turning it off and on again?",
+            "If you type 'Google' into Google, you can break the Internet.",
+            "I came here to drink milk and kick ass... and I've just finished my milk.",
+
+            # Anchorman
+            "I'm very important. I have many leather-bound books and my apartment smells of rich mahogany."
+            "I immediately regret this decision.",
+            "Baxter, is that you? Baxter! Bark twice if you're in Milwaukee.",
+            "I would like to extend to you an invitation to the pants party.",
+            "Don't act like you're not impressed.",
+            "60 percent of the time, it works every time.",
+            "I'm in a glass case of emotion!",
+            "You know I don't speak Spanish.",
+            "I love lamp."
 
         ]
         response = random.choice(responses)
