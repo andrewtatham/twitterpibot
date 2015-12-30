68236b9633b4498e26f95a1621a8aeb2f20d0982
Andrew Tatham <andrewtatham@gmail.com>
2015-12-30 12:05:00 +0000

    logging changes

--- twitterpibot/MyLogging.py
+++ twitterpibot/MyLogging.py
@@ -10,18 +10,30 @@ dir = "temp" + os.sep + "log" + os.sep
 if not os.path.exists(dir):
     os.makedirs(dir)
 
-fh = handlers.RotatingFileHandler(dir + "twitter.log", maxBytes=4 * 1024 * 1024, backupCount=2)
-fh.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+debug_file_log = handlers.RotatingFileHandler(dir + "twitter_debug.log", maxBytes=4 * 1024 * 1024, backupCount=2)
+debug_file_log.setLevel(logging.DEBUG)
+debug_file_log.setFormatter(formatter)
+rootlogger.addHandler(debug_file_log)
+
+warning_file_log = handlers.RotatingFileHandler(dir + "twitter_warning.log", maxBytes=4 * 1024 * 1024, backupCount=2)
+warning_file_log.setLevel(logging.WARNING)
+warning_file_log.setFormatter(formatter)
+rootlogger.addHandler(warning_file_log)
+
+error_file_log = handlers.RotatingFileHandler(dir + "twitter_error.log", maxBytes=4 * 1024 * 1024, backupCount=2)
+error_file_log.setLevel(logging.ERROR)
+error_file_log.setFormatter(formatter)
+rootlogger.addHandler(error_file_log)
+
+
 # create console handler with a higher log level
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
-# create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
-fh.setFormatter(formatter)
-# add the handlers to rootlogger
 rootlogger.addHandler(ch)
-rootlogger.addHandler(fh)
 
 
 def init():
