bd17592047141e35af33dde937314ddd1530e87c
Andrew Tatham <andrewtatham@gmail.com>
2016-01-26 23:30:28 +0000

    removed back off

--- twitterpibot/ExceptionHandler.py
+++ twitterpibot/ExceptionHandler.py
@@ -15,9 +15,9 @@ logger = logging.getLogger(__name__)
 # twython.exceptions.TwythonRateLimitError: Twitter API returned a 429 (Too Many Requests), Rate limit exceeded
 # TwythonError: HTTPSConnectionPool(host='http://api.twitter.com ', port=443): Read timed out. (read timeout=None)
 # TwythonError: ('Connection aborted.', error(110, 'Connection timed out'))
-
-_back_off_default = 2
-_back_off_seconds = _back_off_default
+#
+# _back_off_default = 2
+# _back_off_seconds = _back_off_default
 
 
 def handle_silently(exception):
@@ -51,13 +51,13 @@ def _try_send_exception():
     try:
         if hardware.is_linux:
             send(OutgoingDirectMessage(text=traceback.format_exc()))
-            _back_off_seconds = _back_off_default
+            # _back_off_seconds = _back_off_default
     except Exception as e:
         logger.exception(e)
-        _back_off_seconds()
+        # _back_off()
 
 
-def _back_off():
-    global _back_off_seconds
-    time.sleep(_back_off_seconds)
-    _back_off_seconds = min(9000, _back_off_seconds * 2)
+# def _back_off():
+#     global _back_off_seconds
+#     time.sleep(_back_off_seconds)
+#     _back_off_seconds = min(256, _back_off_seconds * 2)
