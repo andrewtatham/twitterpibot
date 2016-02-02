1161266ae0d5828b94d85e69eb38fbfa6197d990
Andrew Tatham <andrewtatham@gmail.com>
2016-02-02 21:33:18 +0000

    bot blocker topics

--- twitterpibot/twitter/topics/Spam.py
+++ twitterpibot/twitter/topics/Spam.py
@@ -11,10 +11,9 @@ class TooKeen(SpamTopic):
 class SmutSpam(SpamTopic):
     def __init__(self):
         super(SmutSpam, self).__init__(
-            ["kinky", "frisky", "cum", "horny", "housewi(fe|ves?)",
-             "boobs?", "tits?", "puss(y|ies)",
-             "milf", "hoes?", "boot(y|ies)", "18+", "xxx", "slut", "babe", "bitch"],
-            ["latina", "sexy", "ass", "dirty", "naked", "naughty", "bored", "teen", "sex"]
+            ["kinky", "frisky", "cum", "horny", "housewi(fe|ves?)", "boobs?", "tits?", "puss(y|ies)",
+             "milf", "hoes?", "boot(y|ies)", "18\+", "slut", "bitch"],
+            ["latina", "sexy", "ass", "dirty", "naked", "naughty", "bored", "teen", "sex", "xxx", "babe"]
         )
 
 
