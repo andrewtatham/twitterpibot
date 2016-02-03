e6fddf37c3172d7757100e2a3b03f9badc18c70e
Andrew Tatham <andrewtatham@gmail.com>
2016-02-03 09:57:59 +0000

    wikipedia misconceptions

--- twitterpibot/logic/WikipediaWrapper.py
+++ twitterpibot/logic/WikipediaWrapper.py
@@ -3,20 +3,73 @@ import logging
 
 import wikipedia
 
+
+def _parse_heading(line):
+    return "=" in line, line
+
+
+def _parse_content_tree(page_content):
+    current_heading = "None"
+    content_tree = {current_heading: []}
+    for line in page_content.split('\n'):
+        if line:
+
+            is_heading, heading = _parse_heading(line)
+            if is_heading:
+                current_heading = heading
+                content_tree[current_heading] = []
+            else:
+                content_tree[current_heading].append(line)
+                # print(line)
+
+    return content_tree
+
+
+def _parse_content_flat(page_content):
+    content_flat = []
+    tree = _parse_content_tree(page_content)
+    skip_sections = {
+        "None",
+        "== External links ==",
+        "== Further reading ==",
+        "== Notes ==",
+        "== References ==",
+        "== See also =="
+    }
+
+    for title, section in tree.items():
+        if title not in skip_sections:
+            content_flat.extend(section)
+
+    return content_flat
+
+
+def get_random_page():
+    # https://wikipedia.readthedocs.org/en/latest/quickstart.html
+    random_title = wikipedia.random(pages=1)
+    random_page = None
+    while not random_page:
+        try:
+            random_page = wikipedia.page(title=random_title)
+        except wikipedia.PageError:
+            random_title = wikipedia.random(pages=1)
+            random_page = None
+        except wikipedia.DisambiguationError as e:
+            random_title = random.choice(e.options)
+            random_page = None
+    return random_page
+
+
 logger = logging.getLogger(__name__)
 
+misconceptions = _parse_content_flat(wikipedia.page("List of common misconceptions").content)
 
-def GetRandomPage():
-    # https://wikipedia.readthedocs.org/en/latest/quickstart.html
-    rand = wikipedia.random(pages=1)
-    page = None
-    while not page:
-        try:
-            page = wikipedia.page(title=rand)
-        except wikipedia.PageError:
-            rand = wikipedia.random(pages=1)
-            page = None
-        except wikipedia.DisambiguationError as e:
-            rand = random.choice(e.options)
-            page = None
-    return page
+
+def get_random_misconception():
+    return random.choice(misconceptions)
+
+
+if __name__ == "__main__":
+
+    for i in range(3):
+        print(get_random_misconception())
--- twitterpibot/schedule/WikipediaScheduledTask.py
+++ twitterpibot/schedule/WikipediaScheduledTask.py
@@ -1,24 +1,19 @@
 import logging
 import os
 import pprint
+import random
+import textwrap
 
 from apscheduler.triggers.cron import CronTrigger
 
 from twitterpibot.logic import WikipediaWrapper, FileSystemHelper
 from twitterpibot.schedule.ScheduledTask import ScheduledTask
 from twitterpibot.outgoing.OutgoingTweet import OutgoingTweet
-from twitterpibot.twitter import TwitterHelper
 from twitterpibot.twitter.TwitterHelper import send
 
-link = 21
-space = 1
-ellipsis = 3
-length = 140-(link + space + ellipsis)
 
 def cap(s, l):
     return s if len(s) <= l else s[0:l - 3] + '...'
-
-
 
 
 logger = logging.getLogger(__name__)
@@ -27,26 +22,54 @@ folder = "temp" + os.sep + "wikipedia" + os.sep
 FileSystemHelper.ensure_directory_exists(folder)
 
 
+def tweet_random_wikipedia_page():
+    page = WikipediaWrapper.get_random_page()
+    if page:
+
+        text = cap(page.url + " " + page.summary, 140)
+
+        file_paths = None
+        if any(page.images):
+            # filter to PNG, JPEG, WEBP and GIF.
+            images = list(filter(lambda url: FileSystemHelper.check_extension(url), page.images))
+            if any(images):
+                pprint.pprint(images)
+                url = images[0]
+                path = FileSystemHelper.download_file(folder, url)
+                file_paths = [path]
+
+        send(OutgoingTweet(text=text, file_paths=file_paths))
+
+
+def tweet_random_misconception():
+    reply_to_id = None
+    misconception = WikipediaWrapper.get_random_misconception()
+    lines = textwrap.wrap(misconception, 140 - 6)
+    lines_count = len(lines)
+    line_number = 0
+
+    for line in lines:
+        is_continuation = lines_count > 1 and line_number != 0
+        has_continuation = lines_count > 1 and line_number != lines_count - 1
+        text = ""
+        if is_continuation:
+            text += "..."
+        text += line
+        if has_continuation:
+            text += "..."
+        reply_to_id = send(OutgoingTweet(text=text, in_reply_to_status_id=reply_to_id))
+        line_number += 1
+
+
 class WikipediaScheduledTask(ScheduledTask):
     def GetTrigger(self):
-        return CronTrigger(hour="*")
+        return CronTrigger(hour="*", minute="15,45")
 
     def onRun(self):
-        page = WikipediaWrapper.GetRandomPage()
-
-        if page:
-            text = cap(page.url + " " + cap(page.summary, length), 140)
-            file_paths = None
-            if any(page.images):
-                # filter to PNG, JPEG, WEBP and GIF.
-                images = list(filter(lambda url: FileSystemHelper.check_extension(url), page.images))
-                if any(images):
-                    pprint.pprint(images)
-                    url = images[0]
-                    path = FileSystemHelper.download_file(folder, url)
-                    file_paths = [path]
-
-            send(OutgoingTweet(text=text, file_paths=file_paths))
+        if random.randint(0, 1) == 0:
+            tweet_random_misconception()
+        else:
+            tweet_random_wikipedia_page()
 
 
 if __name__ == "__main__":
