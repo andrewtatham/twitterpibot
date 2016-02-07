5f3de1501743af44eb7789d5af736c41902459d7
Andrew Tatham <andrewtatham@gmail.com>
2016-02-07 23:25:23 +0000

    changed markov library

--- twitterpibot/logic/markovhelper.py
+++ twitterpibot/logic/markovhelper.py
@@ -1,7 +1,9 @@
-from pymarkov import markov
+import os
+import pprint
 
-ply = 3
-length = 20
+import markovgen
+
+from twitterpibot.logic import webscraper
 
 
 class MarkovResult(object):
@@ -9,8 +11,15 @@ class MarkovResult(object):
         self.markov_dict = markov_dict
 
     def speak(self):
-        return markov.generate(self.markov_dict, length, ply)
+        return self.markov_dict.generate_markov_text()
 
 
 def train(text):
-    return MarkovResult(markov.train(text, ply))
+    markov_dict = markovgen.Markov()
+    markov_dict.feed(text)
+    # pprint.pprint(markov_dict.generate_markov_text())
+    return MarkovResult(markov_dict)
+
+
+if __name__ == "__main__":
+    pprint.pprint(train(os.linesep.join(webscraper.get_malcolm_tucker_quotes())).speak())
