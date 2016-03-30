a647913a34efe8de9eee31fd200cef38eb391850
Andrew Tatham <andrewtatham@gmail.com>
2016-03-30 08:52:00 +0100

    db

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import os
 import colorama
 
 from twitterpibot import hardware
-if hardware.is_raspberry_pi_2:
-    from twitterpibot.logic import fsh
-    fsh.delete_files([
-        fsh.root + "temp/db/tokens.db",
-        fsh.root + "temp/db/twitterpibot.db"
-    ])
-    from twitterpibot.data_access import dal
-    dal.import_tokens("tokens.csv")
 
-import twitterpibot
+# from twitterpibot.logic import fsh
+# from twitterpibot.data_access import dal
+# if hardware.is_raspberry_pi_2:
+#     fsh.delete_files([
+#         fsh.root + "temp/db/tokens.db",
+#         fsh.root + "temp/db/twitterpibot.db"
+#     ])
+#     dal.import_tokens("tokens.csv")
+
 from twitterpibot.logic.april_fools_day import AprilFoolsDayScheduledTask
 from twitterpibot.logic.botgle import BotgleResponse
 from twitterpibot.logic.conversation import ConversationScheduledTask
