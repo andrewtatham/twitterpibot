ac9d6ca4260879e1c57a65b0855e2999c4cf1ec2
Andrew Tatham <andrewtatham@gmail.com>
2016-04-15 04:34:06 +0100

    emoji

--- twitterpibot/logic/webscraper.py
+++ twitterpibot/logic/webscraper.py
@@ -33,9 +33,15 @@ def get_emojis():
             if cells:
                 if len(cells) >= 15:
                     name = cells[14].text
+                    year = cells[15].text
+                    year = re.match("[0-9]+",year).string[:4]
+                    year = int(year)
+
+
                     if "," not in name \
                             and "Keycap" not in name \
-                            and "Flag" not in name:
+                            and "Flag" not in name\
+                            and year <=2013:
 
                         if "≊" in name:
                             name = name[:name.index("≊")]
