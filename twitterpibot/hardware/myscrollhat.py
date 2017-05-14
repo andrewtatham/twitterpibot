bde18252d0178bdb3450e9e586006df19b776b48
Andrew Tatham <andrewtatham@gmail.com>
2017-05-14 08:40:07 +0100

    scrollbot

--- twitterpibot/hardware/myscrollhat.py
+++ twitterpibot/hardware/myscrollhat.py
@@ -23,10 +23,12 @@ def lights():
         time.sleep(1)
 
     if status_length <= 0 and any(q):
+        scrollphathd.clear()
+        scrollphathd.show()
         logging.info("myscrollhat len(q) = {}".format(len(q)))
         status = q.pop()
         scrollphathd.write_string(status, font=font5x7smoothed, brightness=0.1)
-        status_length = scrollphathd.write_string(status, x=0, y=0, font=font5x7smoothed, brightness=0.1)
+        status_length = 17 + scrollphathd.write_string(status, x=0, y=0, font=font5x7smoothed, brightness=0.1)
 
 
 def inbox_item_received(inbox_item):
