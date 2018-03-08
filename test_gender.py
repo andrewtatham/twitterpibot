a5c0ea5e3c60ee9d495dbfa96ad55f9ae49aa359
Andrew Tatham <andrewtatham@gmail.com>
2018-03-08 20:49:40 +0000

    mens day

---
+++ test_gender.py
@@ -0,0 +1,90 @@
+from unittest import TestCase
+
+import twitterpibot.logic.gender as gender
+
+__author__ = 'andrewtatham'
+
+question_testcases = [
+    "We want international men’s day #InternationalMensDay",
+    " Why there is no #InternationalMensDay ?",
+    " #InternationalMensDay when will this be allowed?",
+    " Do we need #InternationalMensDay then to even up just saying",
+    " Why isn't there a men's day?",
+    " There is no \"International Men's Day\" for the ",
+    " when is international men’s day?",
+    " Besides, when exactly is International Men’s Day?",
+    "So when is men's day?",
+    " Why isn't there an International Men's Day?",
+    "  Is there even a international men's day? Lol",
+    " Where’s the international men’s day?",
+    " Is there an international men's day?"
+    "Is there a international men's day?",
+    "I must have missed it , but when is international men's day?",
+    "Imagine the outcry if someone decided on a international mens day",
+    "When is 'International mens day?'",
+    "Out of interest when is International Mens Day? Asking for friends.",
+    "I, for one, would like to take this opportunity to ask, WHEN IS INTERNATIONAL MENS DAY? WHAT ABOUT THE MEN? yall talk about equality smh",
+    "I see Womens Day nice nice feminists now WHEN THE FUCK IS MENS DAY?",
+    "Someone enlighten me pls why is there no international mens day",
+    "Serious Question and please excuse my ignorance in advance - Is there such thing as International Mens Day? If so, when is it?",
+    "Love the idea of international woman's day. In the interest of equality, what date is international mens day?",
+    "#InternationalWomansDay where’s international mens day???!!!??? #sexist",
+    "BuT wHaT aBoUt InTeRnAtIoNaL mEnS dAy??? ",
+    "When its international mens day im getting a meriton with two occas to celebrate every cunt from the area is invited",
+    "Imagine the backlash over a international mens day lol",
+    "When’s national mens day? Girls get one, shouldn’t we?",
+    "When is it national mens day ?",
+    "Is there an official National Mens Day? If there isn't there needs to be one!",
+    "What about international MENS day huh? Sounds like sexism if you ask me!",
+    "When’s international mens day cos I’m feeling left out",
+    "what is the date of international mens day?",
+    "How about an #internationalmensday you sexist bitches!   "
+
+]
+
+not_question_testcases = [
+    "Every day is international mens day you ball scratching, crying, self centred hypochondriacs ",
+    " its international woman's day today! Husband- so whens international mens day.... Me-........ Husband-......Me- um.. All the other 364 days of the fucking year.",
+    "More Whose idea was it to to have #InternationalWomensDay on the same day as International Insecure Mens day?",
+    "Intl. Mens Day is Monday the 19th of November Mr. Caller Man there. #liveline",
+    "If u ask me when international mens day is, i am gonna #fart",
+    "Mark your Calender, International Mens day on November 19",
+    "International Men’s Day - 19th  November each year.",
+    "Good morning to everyone, except for the men who asked 'When's International Men's Day?'"
+    "Everyday",
+    "Every day",
+    "Every single day",
+    "Every other day",
+    "364",
+    "365",
+    " @Herring1967 "
+]
+
+
+# Hashtags
+# #InternationalWomensDay
+# #iwd2018
+# #InternationalWomenDay2018
+# internationwomensday - WTF spelling
+# #InternationalMensDay
+
+# examples
+
+
+# answers
+# International Men’s Day - 19th  November each year.
+
+
+class TestsGender(TestCase):
+    def test_is_question(self):
+
+        for testcase in question_testcases:
+            with self.subTest(testcase):
+                self.assertTrue(gender.is_question(testcase))
+
+
+    def test_is_not_question(self):
+        for testcase in not_question_testcases:
+            with self.subTest(testcase):
+                self.assertTrue(not gender.is_question(testcase) or gender.is_not_question(testcase))
+
--- twitterpibot/logic/gender.py
+++ twitterpibot/logic/gender.py
@@ -9,6 +9,44 @@ from twitterpibot.schedule.StreamTopicScheduledTask import StreamingTopicSchedul
 
 logger = logging.getLogger(__name__)
 
+_question_rx = re.compile(
+    pattern="When|is.*there|(how|what).*about|Why.*(no|isn't)|We want|Do we need|There is no|Where|What|backlash|outcry"
+            ".*"
+            "(inter)?national"
+            ".*"
+            "men'?s.*day"
+            ".*"
+            "\??" # literal question mark, optional
+    ,
+    flags=re.IGNORECASE)
+
+_answer_rx_1 = re.compile(
+    "(19|nineteen)|(11|Nov)"
+      "|Every( ?| single | other )day"
+    "|364|365"
+    "|ask"
+    "|insecure"
+    "|@Herring1967"
+    , flags=re.IGNORECASE)
+
+
+def is_question(text):
+    return bool(_question_rx.findall(text))
+
+def is_not_question(text):
+    return bool(_answer_rx_1.findall(text))
+
+
+# responses
+# @UKMensDay
+# @Herring1967
+
+# Let me google that for you http://lmgtfy.com/?q=When+is+international+mens+day
+
+# Make a donation to https://www.justgiving.com/fundraising/November19th?
+
+responses = ["International Men's Day is on November 19th"]
+
 
 class WhenIsIMDScheduledTask(StreamingTopicScheduledTask):
     def __init__(self, identity):
@@ -22,38 +60,27 @@ class WhenIsIMDScheduledTask(StreamingTopicScheduledTask):
 
     def _get_topic_streamer(self):
         return self.identity.twitter.get_streamer(
-            topic="international men day",
+            topic="men,day",
             topic_name=self._task_key,
             responses=[WhenIsInternationalMensDayResponse(self.identity)],
             filter_level="none"
         )
 
 
-responses = ["International Men's Day is on November 19th"]
-
-
 class WhenIsInternationalMensDayResponse(Response):
     def __init__(self, identity):
         super(WhenIsInternationalMensDayResponse, self).__init__(identity)
-        self._question_rx = re.compile(
-            pattern="(When|is.*there|(how|what).*about).*international.*men'?s.*day\?",
-            flags=re.IGNORECASE)
-
-        self._answer_rx_1 = re.compile("19|nineteen", flags=re.IGNORECASE)
-        self._answer_rx_2 = re.compile("11|Nov", flags=re.IGNORECASE)
 
     def condition(self, inbox_item):
         # when in quotes its rhetorical so ignore, also answers were sometimes images
         return (inbox_item.is_tweet or inbox_item.is_direct_message) \
-               and not inbox_item.has_media \
-               and '"' not in inbox_item.text \
-               and bool(self._question_rx.findall(inbox_item.text)) \
-               and not (bool(self._answer_rx_1.findall(inbox_item.text))
-                        and bool(self._answer_rx_2.findall(inbox_item.text)))
+               and is_question(inbox_item.text) \
+               and not is_not_question(inbox_item.text)
 
     def respond(self, inbox_item):
         self.identity.statistics.increment("International womens/mens day tweets")
         response = random.choice(responses)
+
         self.identity.twitter.quote_tweet(
             inbox_item,
             text=response)
